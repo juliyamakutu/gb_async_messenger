9dd0be675d33df7e351faeb1af5a323eb425827f
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-07-20 17:23:38 +0200

    lesson 16: cx_Freeze setup for cross platform

--- src/setup_server_exe.py
+++ src/setup_server_exe.py
@@ -1,13 +1,17 @@
+import sys
+
 from cx_Freeze import Executable, setup
 
 build_exe_options = {
     "packages": ["config", "server", "common", "db", "log"],
 }
 
+base = "Win32GUI" if sys.platform == "win32" else None
+
 setup(
     version="0.1",
     name="AGBM Server",
     description="GeekBrains Study Project Server",
     options={"build_exe": build_exe_options},
-    executables=[Executable("server.py", base="Win32GUI", target_name="server.exe")],
+    executables=[Executable("server/server.py", base=base, target_name="server.exe")],
 )
